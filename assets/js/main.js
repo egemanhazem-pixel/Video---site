7f46ad77ee7d090eb8fc0b902823adf3237c7f79
egemanhazem-pixel <egemanhazem@gmail.com>
2026-01-11 09:26:28 +0200

    Add video fetching and display functionality

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+fetch("data/videos.json")
+  .then(res => res.json())
+  .then(videos => {
+    const grid = document.getElementById("videosGrid");
+
+    videos.forEach(video => {
+      grid.innerHTML += `
+        <div class="card">
+          <a href="video.html?id=${video.id}">
+            <img src="${video.thumbnail}">
+            <h3>${video.title}</h3>
+          </a>
+        </div>
+      `;
+    });
+  });
